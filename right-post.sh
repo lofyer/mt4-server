a5b4422a728a3dbfa357c8690ec1d371ca204c21
lofyer <lofyer@gmail.com>
2016-11-03 15:14:24 +0800

    update

---
+++ right-post.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X POST -H "'Content-type':'application/x-www-form-urlencoded', 'charset':'utf-8', 'Accept': 'text/plain'" -d "jiojsd" 'http://localhost:8888/post.php?fname=oijio&lname=oijejf'
