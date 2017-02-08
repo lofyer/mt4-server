56bf2e01582566cdb4901b052845abba03a017b0
lofyer <lofyer@gmail.com>
2017-02-08 15:29:43 +0800

    port added

--- flask-main.py
+++ flask-main.py
@@ -100,4 +100,4 @@ def uploaded_file(filename):
 
 if __name__ == '__main__':
     app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-    app.run(host='0.0.0.0', debug = True)
+    app.run(host='0.0.0.0', port=5000, debug = True)
